54f61d9ef541dd553082df763a184a4247149746
apple <apple@rsk-macbook.local>
2014-12-16 17:33:11 +0530

    Initial Commit

---
+++ BrtheApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrtheApp
+//
+//  Created by apple on 12/16/14.
+//  Copyright (c) 2014 Brthe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
